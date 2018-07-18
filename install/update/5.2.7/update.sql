df90b4609ccf8f620e6d3b78ff343a2562e747fe
Travis CI User <travis@example.org>
2018-07-18 09:54:32 +0000

    Upd

--- install/update/5.2.7/update.sql
+++ install/update/5.2.7/update.sql
@@ -28,3 +28,7 @@ ALTER TABLE `phpshop_servers` ADD `adres` varchar(255) default '';
 ALTER TABLE `phpshop_servers` ADD `logo` varchar(255) default '';
 ALTER TABLE `phpshop_servers` ADD `adminmail` varchar(255) default '';
 ALTER TABLE `phpshop_order_status` ADD `mail_message` text default '';
+ALTER TABLE `phpshop_categories` ADD `sort_cache` blob;
+ALTER TABLE `phpshop_categories` ADD `sort_cache_created_at` int(11);
+ALTER TABLE `phpshop_servers` ADD `currency` int(11);
+ALTER TABLE `phpshop_servers` ADD `lang` varchar(32);
--- install/update/5.3.1/update.sql
+++ install/update/5.3.1/update.sql
@@ -1,2 +1,6 @@
 ALTER TABLE `phpshop_1c_docs` ADD `year` int(11) default 2018;
 ALTER TABLE `phpshop_orders` ADD `files` text;
+ALTER TABLE `phpshop_categories` ADD `sort_cache` blob;
+ALTER TABLE `phpshop_categories` ADD `sort_cache_created_at` int(11);
+ALTER TABLE `phpshop_servers` ADD `currency` int(11);
+ALTER TABLE `phpshop_servers` ADD `lang` varchar(32);
